baa4280ebf3f8664dd74b536eecee4a3ca8d23ba
Kristina Fischer <kmfischer3@wisc.edu>
2016-06-11 23:29:42 -0500

    potentially fixes _SER error

--- sandbox/login.php
+++ sandbox/login.php
@@ -3,30 +3,7 @@
 <?php require_once("../includes/functions.php"); ?>
 <?php 
 
-// Set up JSON response per the API specs
-$response = array();
-
-$response["status_code"] = "_UNKNOWN";
-
-if (mysqli_connect_errno()) {
-	$response["status_code"] = "_SER";
-} else {
-	$query = "SELECT * FROM Users";
-	$result = mysqli_query($connection, $query);
-	if (!$result) {
-		$response["status_code"] = "_SQL";
-	} else {
-		while ($person = mysqli_fetch_assoc($result)) {
-			array_push($response, $person);
-		}
-		$_SESSION["userid"] = "1";
-		echo "session userid = ".$_SESSION["userid"].<br>;
-		$response["status_code"] = "OK";
-	}	
-}
-
-// Output pretty JSON
-$json = json_encode($response);
-echo $json;
-mysql_close($connection);
+echo "hello";
+$_SESSION["userid"] = "1";
+echo "session userid = ".$_SESSION["userid"];
 ?>
