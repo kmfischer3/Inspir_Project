5800e4a113afd04ff40541077e9fcc05c8d6c7c2
Kristina Fischer <kmfischer3@wisc.edu>
2016-06-11 23:49:43 -0500

    redirect to index.php upon login

--- sandbox/validate_login.php
+++ sandbox/validate_login.php
@@ -2,33 +2,7 @@
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
 <?php 
-
-// Set up JSON response per the API specs
-$response = array();
-
-$response["status_code"] = "_UNKNOWN";
-
-if (mysqli_connect_errno()) {
-	$response["status_code"] = "_SER";
-} else {
-	$query = "SELECT * FROM Content";
-	$result = mysqli_query($connection, $query);
-	if (!$result) {
-		$response["status_code"] = "_SQL";
-	} else {
-		while ($person = mysqli_fetch_assoc($result)) {
-			/*$id = $result['id'];
-			$name = $result['name'];
-			$age = $result['age'];
-			$nickname = 0;*/
-			array_push($response, $person);
-		}
-		$response["status_code"] = "OK";
-	}	
-}
-
-// Output pretty JSON
-$json = json_encode($response);
-echo $json;
-mysql_close($connection);
+	$_SESSION["userid"] = "1";
+	echo "session user id = ".$_SESSION["userid"];
+	header('Location: http://pages.cs.wisc.edu/~kristina/inspir_project/sandbox/index.php');
 ?>
