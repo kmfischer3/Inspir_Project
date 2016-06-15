a6a8e7179fa07c628dd837b340512f643eb0bfaf
Kristina Fischer <kmfischer3@wisc.edu>
2016-06-14 20:15:22 -0500

    test

--- sandbox/validate_login.php
+++ sandbox/validate_login.php
@@ -4,26 +4,27 @@
 <?php 
 
 if ( !empty($_GET["login"]) && !empty($_GET["password"]) ) {
-	$login = $_GET["login"];
-	$password = $_GET["password"];
+	// $login = $_GET["login"];
+	// $password = $_GET["password"];
 
 
-	if (mysqli_connect_errno()) {
-		echo "_SER error";
-	} else {
-		$query = "SELECT password FROM Users WHERE login='{$login}';";
-		$result = mysqli_query($connection, $query);
-		if (!$result) {
-			echo = "_SQL error";
-		} else {
-			if ($password == $result) {
-				$id_query = "SELECT id FROM Users WHERE login='{$login}';";
-				$id = mysqli_query($connection, $id_query);
-				$_SESSION["userid"] = "{$id}";
-				header('Location: http://pages.cs.wisc.edu/~kristina/inspir_project/sandbox/index.php');				
-			}
-		}	
-	}
+	// if (mysqli_connect_errno()) {
+	// 	echo "_SER error";
+	// } else {
+	// 	$query = "SELECT password FROM Users WHERE login='{$login}';";
+	// 	$result = mysqli_query($connection, $query);
+	// 	if (!$result) {
+	// 		echo = "_SQL error";
+	// 	} else {
+	// 		if ($password == $result) {
+	// 			$id_query = "SELECT id FROM Users WHERE login='{$login}';";
+	// 			$id = mysqli_query($connection, $id_query);
+	// 			$_SESSION["userid"] = "{$id}";
+	// 			header('Location: http://pages.cs.wisc.edu/~kristina/inspir_project/sandbox/index.php');				
+	// 		}
+	// 	}	
+	// }
+	echo "success";
 
 } else {
 	echo "fail";
